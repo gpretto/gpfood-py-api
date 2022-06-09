b880c1d3f77b8ad360c75ce733a2be19626a4b66
gpretto <guilherme.pretto@fast2mine.com>
2022-06-09 17:58:51 -0300

    Add tests and implemented function wait_for_db (fix race condition)

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,27 @@
+"""
+Django commands to wait for the database to be available.
+"""
+import time
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django commands to wait for database."""
+
+    def handle(self, *args, **options):
+        """Entrypoint for command."""
+        self.stdout.write('Waiting for database...')
+        dp_up = False
+        while dp_up is False:
+            try:
+                self.check(databases=['default'])
+                dp_up = True
+            except (Psycopg2Error, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database available.'))
